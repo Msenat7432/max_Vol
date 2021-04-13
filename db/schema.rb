4fb228bc4a442b0b7e074323e3add88bfa5329bb
msenat7432 <maxsenat7432@gmail.com>
2021-04-12 23:16:43 -0400

    mac-vol

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,4 @@ ActiveRecord::Schema.define(version: 2021_04_12_014843) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-   create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.string "artist"
-  end
-
 end
